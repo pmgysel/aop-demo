bd13dde06ae1d4545e5fcb3e69c6c5664f42dc21
Philipp Gysel <pmgysel@ucdavis.edu>
2021-01-01 17:07:16 +0000

    advice loggs method name in generic fashion

--- src/main/java/com/example/aop/aspect/LoggingAspect.java
+++ src/main/java/com/example/aop/aspect/LoggingAspect.java
@@ -1,5 +1,6 @@
 package com.example.aop.aspect;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -8,7 +9,8 @@ import org.springframework.stereotype.Component;
 @Component
 public class LoggingAspect {
   @Before("@annotation(com.example.aop.LogMethod)")
-  public void logMethod(){
-    System.out.println("Method \"restCall\" was called");
+  public void logMethod(JoinPoint joinPoint){
+    String method = joinPoint.getSignature().getName();
+    System.out.println("Method " + method + " was called");
   }
 }
