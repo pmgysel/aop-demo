a85d1bc5ac75c84df261c28e663c7b8e9400e272
Philipp Gysel <pmgysel@ucdavis.edu>
2021-01-01 17:40:51 +0000

    2 aspects: one for logging method name and params, one for logging execution time.

--- src/main/java/com/example/aop/MyRestCall.java
+++ src/main/java/com/example/aop/MyRestCall.java
@@ -1,13 +1,20 @@
 package com.example.aop;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyRestCall {
   @LogMethod
-  @RequestMapping(path = "/api/get")
-  public String restCall() {
-    return "hello world";
+  @RequestMapping(path = "/api/greeting/{name}")
+  public String greeting(@PathVariable(value = "name") String name) {
+    return "Hello " + name + "!";
+  }
+
+  @LogMethod
+  @RequestMapping(path = "/api/order/{menu}")
+  public String order(@PathVariable(value = "menu") String menu) {
+    return "You placed an order for the menu " + menu + "!";
   }
 }
--- src/main/java/com/example/aop/aspect/LoggingAspect.java
+++ src/main/java/com/example/aop/aspect/LoggingAspect.java
@@ -1,16 +1,32 @@
 package com.example.aop.aspect;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+
 @Aspect
 @Component
 public class LoggingAspect {
+
   @Before("@annotation(com.example.aop.LogMethod)")
-  public void logMethod(JoinPoint joinPoint){
+  public void logMethodName(JoinPoint joinPoint) {
     String method = joinPoint.getSignature().getName();
-    System.out.println("Method " + method + " was called");
+    String params = Arrays.toString(joinPoint.getArgs());
+    System.out.println("Method [" + method + "] gets called with parameters " + params);
   }
+
+  @Around("@annotation(com.example.aop.LogMethod)")
+  public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+    long startTime = System.currentTimeMillis();
+    Object proceed = joinPoint.proceed();
+    long duration = System.currentTimeMillis() - startTime;
+    System.out.println("Exeution took [" + duration + "ms]");
+    return proceed;
+  }
+
 }
